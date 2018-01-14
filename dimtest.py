ed751782e1b9780e13fdfe3de358839a7f7c0666
seekatar <seekatar@gmail.com>
2018-01-14 16:23:30 -0500

    fading working

--- dimtest.py
+++ dimtest.py
@@ -5,18 +5,55 @@ from scrollphathd.fonts import font5x5
 
 hat.rotate(180)
 
+BRIGHTNESS = .25
+BRIGHTERNESS = .7
+
+def fade_text(curr_text: str, next_text: str, fade_time: int):
+    """ fade from one text to another
+    """
+    curr_brightness = BRIGHTNESS
+    second_brightness = 0
+    sleep_time = fade_time / 100.0
+    step = .01
+    range_limit = int(BRIGHTNESS / step)
+    sleep_time = fade_time / range_limit
+
+    for i in range(range_limit):
+        curr_brightness -= step
+        if curr_brightness < 0:
+            curr_brightness = 0
+        second_brightness += step
+
+        if curr_brightness < second_brightness:
+            hat.write_string(curr_text, brightness=curr_brightness, y=1, font=font5x5)
+            hat.write_string(next_text, brightness=second_brightness, y=1, font=font5x5)
+        else:
+            hat.write_string(next_text, brightness=second_brightness, y=1, font=font5x5)
+            hat.write_string(curr_text, brightness=curr_brightness, y=1, font=font5x5)
+        hat.show()
+        time.sleep(sleep_time)
+
+
 first_string = "12:34"
-firstBrightness = 1.0
+firstBrightness = .25
 second_string = " 34"
 second_brightness = 0.0
 
+hat.set_pixel(0,6,.5)
 hat.set_pixel(1,6,.5)
 hat.set_pixel(2,6,.5)
 hat.set_pixel(3,6,.5)
 hat.show()
-time.sleep(2)
-for i in range(99):
+
+fade_text(first_string, second_string, 1)
+time.sleep(3)
+fade_text(second_string, first_string, 1)
+time.sleep(3)
+
+for i in range(25):
     firstBrightness -= .01
+    if firstBrightness < 0:
+        break
     second_brightness += .01
     if firstBrightness < second_brightness:
         hat.write_string(first_string, brightness=firstBrightness, y=1,font=font5x5)
--- slackscrollbot.py
+++ slackscrollbot.py
@@ -11,23 +11,27 @@ hat.rotate(180)
 BRIGHTNESS = .25
 BRIGHTERNESS = .7
 
-def fade_text(curr_text: str, next_text: str, fade_time: int):
+def fade_text(curr_text: str, next_text: str, curr_x: int=1, next_x: int=1,fade_time: int=1):
     """ fade from one text to another
     """
     curr_brightness = BRIGHTNESS
     second_brightness = 0
-    sleep_time = fade_time / 100.0
-    step = BRIGHTNESS / 100.0
+    step = .01
+    range_limit = int(BRIGHTNESS / step)
+    sleep_time = fade_time / range_limit
 
-    for i in range(100):
+    for i in range(range_limit):
         curr_brightness -= step
+        if curr_brightness < 0:
+            curr_brightness = 0
         second_brightness += step
+
         if curr_brightness < second_brightness:
-            hat.write_string(curr_text, brightness=curr_brightness, y=1, font=font5x5)
-            hat.write_string(next_text, brightness=second_brightness, y=1, font=font5x5)
+            hat.write_string(curr_text, brightness=curr_brightness, x=curr_x, y=1, font=font5x5)
+            hat.write_string(next_text, brightness=second_brightness, x=next_x, y=1, font=font5x5)
         else:
-            hat.write_string(curr_text, brightness=second_brightness, y=1, font=font5x5)
-            hat.write_string(next_text, brightness=curr_brightness, y=1, font=font5x5)
+            hat.write_string(next_text, brightness=second_brightness, x=next_x, y=1, font=font5x5)
+            hat.write_string(curr_text, brightness=curr_brightness, x=curr_x, y=1, font=font5x5)
         hat.show()
         time.sleep(sleep_time)
 
@@ -53,18 +57,17 @@ def show_unreads(prev_count, new_count):
 
 def get_time_str():
     time_string = time.strftime("%I:%M")
+    x_coord = 0
     if time_string[0] == '0':
         time_string = time_string[1:]
-    return time_string
+        x_coord = 4
+    return time_string,x_coord
 
 def show_time():
     """ show the time
     """
-    hat.clear_rect(0, 0, 17, 6)
-    x_coord = 0
-    time_string = get_time_str()
-    if len(time_string) == 4:
-        x_coord = 4
+    hat.clear_rect(0, 1, 17, 6)
+    time_string,x_coord = get_time_str()
     hat.write_string(time_string,
                      x=x_coord, y=1,
                      font=font5x5,
@@ -76,21 +79,22 @@ def show_time():
     if now % 2 == 0:
         hat.clear_rect(8, 1, 1, 5)
     hat.show()
-    return time_string
+    return time_string,x_coord
 
 def get_temp_str(temperature):
-    return " "+int(temperature)
+    return " "+str(round(temperature)),0
 
 def show_temp(temperature):
     """ show the current temperature
     """
-    hat.clear_rect(0, 0, 17, 6)
-    hat.write_string(get_temp_str(temperature),
+    hat.clear_rect(0, 1, 17, 6)
+    temp_str,x_coord = get_temp_str(temperature)
+    hat.write_string(temp_str,
                      y=1,
                      font=font5x5,
                      brightness=BRIGHTNESS)
 
-    return temperature
+    return temp_str,x_coord
 
 def main():
     """ mainline
@@ -119,25 +123,29 @@ def main():
 
     showing_time = True
     prev_string = ""
+    prev_x = 1
     linger_time = 10
     last_change_time = time.time()
+    last_loop_count = 0
+    loop_brightness = 1
+
     try:
         while True:
 
             if time.time() > last_change_time + linger_time:
                 if showing_time:
-                    next_string = get_temp_str(cw.get_temperature())
+                    next_string,next_x = get_temp_str(cw.get_temperature())
                 else:
-                    next_string = get_time_str()
+                    next_string,next_x = get_time_str()
 
                 showing_time = not showing_time
-                fade_text(prev_string, next_string, 1)
+                fade_text(prev_string, next_string, prev_x, next_x, 1)
                 last_change_time = time.time()
 
             if showing_time:
-                prev_string = show_time()
+                prev_string,prev_x = show_time()
             else:
-                prev_string = show_temp(cw.get_temperature())
+                prev_string,prev_x = show_temp(cw.get_temperature())
 
             new_unreads = poller.get_unread_count()
             if unread_count != new_unreads:
@@ -145,6 +153,13 @@ def main():
                 show_unreads(unread_count, new_unreads)
             unread_count = new_unreads
             time.sleep(.5)
+            loop_count = processor.get_loop_count()
+            if loop_count != last_loop_count:
+                loop_brightness = int(not loop_brightness)
+                last_loop_count = loop_count
+                hat.set_pixel(0, 0, BRIGHTNESS*loop_brightness)
+                hat.show()
+
     except KeyboardInterrupt:
         processor.stop()
 
