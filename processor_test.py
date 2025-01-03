b7e1039a837e2ed6f9d9c1e40077e1b4d65fb1ba
Jim Wallace <seekatar@gmail.com>
2025-01-02 19:42:18 -0500

    RPi running code Jan 2025

--- processor_test.py
+++ processor_test.py
@@ -2,7 +2,7 @@
 import os
 import time
 import processor
-import slackstatus
+# import slackstatus
 import current_weather
 
 class Thrower(processor.Runner):
@@ -14,27 +14,16 @@ class Thrower(processor.Runner):
         raise Exception("ow ow ow")
 
 if __name__ == "__main__":
-    if "SLACK_BOT_TOKEN" in os.environ:
-        slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
-    else:
-        slack_bot_token = None
-
-    if not "SLACK_BOT_WEATHER_KEY" in os.environ:
-        raise ValueError("Must supply SLACK_BOT_WEATHER_KEY in os.envrion")
-
     key = os.environ["SLACK_BOT_WEATHER_KEY"]
     cw = current_weather.CurrentWeather("30022", key, 60)
 
-    if slack_bot_token:
-        slack_poller = slackstatus.SlackPoller(slack_bot_token)
-    else:
-        slack_poller = None
+    # slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
+    # slack_poller = slackstatus.SlackPoller(slack_bot_token)
 
     processor = processor.Processor(True)
 
     processor.add_runner(cw)
-    if slack_bot_token:
-        processor.add_runner(slack_poller)
+    # processor.add_runner(slack_poller)
     processor.add_runner(Thrower())
 
     processor.start()
@@ -42,10 +31,8 @@ if __name__ == "__main__":
     print("Testing, press a key to stop...")
     try:
         while True:
-            if slack_poller:
-                total = slack_poller.get_unread_count()
-            else:
-                total = 0
+            # total = slack_poller.get_unread_count()
+            total = 0
             print(f"[{processor.get_loop_count()}] Total unreads {total} temp = {cw.get_temperature()}")
             time.sleep(5)
     except KeyboardInterrupt:
